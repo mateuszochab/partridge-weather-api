1402ea5aec7c2b81e435d3ebc5d6c07888ee379e
Mateusz Ochab <ochab.mat@gmail.com>
2019-03-13 11:54:16 +0100

    initial commit setup project

---
+++ src/main/java/com/ochabmateusz/partridge/partridgeweatherapi/PartridgeWeatherApiApplication.java
@@ -0,0 +1,13 @@
+package com.ochabmateusz.partridge.partridgeweatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartridgeWeatherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartridgeWeatherApiApplication.class, args);
+	}
+
+}
